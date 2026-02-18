3f10b8e845e858b8277cc4317e4e0d322681b5c5
Arjunatk25 <144626188+Arjunatk25@users.noreply.github.com>
2026-02-18 15:40:16 +0530

    hn

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,11 @@ const defaultCode = `import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  int a=sc.nextInt();
+  int b=sc.nextInt();
   System.out.println("Haaaai soldiers");
+  System.out.println(a+b);
   }
 }`;
 
